58e98b719746d62aab9aafe419bfdfb6d757d07b
Giuseppe-Triolo <144156200+Giuseppe-Triolo@users.noreply.github.com>
2023-10-11 16:16:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const addToList = document.getElementById("addToList");
+addToList.onclick = function () {
+  const table = document.createElement("table");
+  const tr1 = document.createElement("tr");
+  const th1 = document.createElement("th");
+};
