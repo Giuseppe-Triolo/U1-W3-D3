356c4e0bfb2e41e79988b16165dbdf5389a60862
Giuseppe-Triolo <144156200+Giuseppe-Triolo@users.noreply.github.com>
2023-10-11 17:48:05 +0200

    Update script.js
    remade from scratch the task given even if not in time

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-const addToList = document.getElementById("addToList");
-addToList.onclick = function () {
-  const table = document.createElement("table");
-  const tr1 = document.createElement("tr");
-  const th1 = document.createElement("th");
+window.onload = () => {
+  const form = document.getElementById("CreaTask");
+  form.onsubmit = function (e) {
+    e.preventDefault();
+
+    const taskInput = document.getElementById("task");
+    const taskText = taskInput.value;
+    const taskList = document.getElementById("taskList");
+
+    const table = document.createElement("table");
+    const tr = document.createElement("tr");
+    const td = document.createElement("td");
+    const button = document.createElement("button");
+
+    const textClicked = function (e) {
+      e.target.style.textDecoration = "line-through";
+    };
+    button.innerText = "Elimina";
+    td.innerText = taskText;
+    td.addEventListener("click", textClicked);
+    tr.appendChild(td);
+    tr.appendChild(button);
+    table.appendChild(tr);
+    taskList.appendChild(table);
+
+    button.onclick = function (e) {
+      const clickedBtn = e.target;
+      clickedBtn.parentNode.remove();
+    };
+
+    taskInput.value = "";
+  };
 };
